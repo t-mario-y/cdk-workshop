10a40a08c8d1264700bdbd4e758b9630646907a7
t-mario-y <t.mario.yoshi@gmail.com>
2020-01-08 03:13:28 +0900

    cloudwatch eventの追加

--- lib/cdk-workshop-stack.ts
+++ lib/cdk-workshop-stack.ts
@@ -1,13 +1,15 @@
-import cdk = require('@aws-cdk/core');
-import lambda = require('@aws-cdk/aws-lambda');
 import apigw = require('@aws-cdk/aws-apigateway');
+import events = require('@aws-cdk/aws-events');
+import targets = require('@aws-cdk/aws-events-targets');
+import lambda = require('@aws-cdk/aws-lambda');
+import cdk = require('@aws-cdk/core');
 
 export class CdkWorkshopStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     // Lambdaリソースを定義
-    const hello = new lambda.Function(this, 'HelloHandler', {
+    const lambdaHello = new lambda.Function(this, 'HelloHandler', {
       runtime: lambda.Runtime.NODEJS_12_X,
       code: lambda.Code.fromAsset('lambda'),
       handler: 'hello.handler'
@@ -15,7 +17,13 @@ export class CdkWorkshopStack extends cdk.Stack {
 
     //API GatewayでREST APIを定義
     new apigw.LambdaRestApi(this, 'Endpoint', {
-      handler: hello
+      handler: lambdaHello
     });
+
+    // Run every Friday at 6PM UTC
+    const rule = new events.Rule(this, 'Rule', {
+      schedule: events.Schedule.expression('cron(0 18 ? * FRI *)')
+    });
+    rule.addTarget(new targets.LambdaFunction(lambdaHello));
   }
 }
