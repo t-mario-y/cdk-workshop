7db5e82d8d6e60c2aa134aabfac0c97aba788f22
t-mario-y <t.mario.yoshi@gmail.com>
2020-01-08 00:40:17 +0900

    @types/aws-lambdaを導入しanyを消去

--- lambda/hello.ts
+++ lambda/hello.ts
@@ -1,8 +1,10 @@
-exports.handler = async (event: any) => {
+export const handler: AWSLambda.Handler = async (
+  event: AWSLambda.APIGatewayEvent
+) => {
   console.log('request:', JSON.stringify(event, undefined, 2));
   return {
     statusCode: 200,
     headers: { 'Content-Type': 'text/plain' },
-    body: `Hello, CDK! You've hit ${event.path}\n`
+    body: `Hello, CDK! You've hit ${event.path}`
   };
 };
